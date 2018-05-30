6e3b39a78fdbb4465cd1870740157158aba8c5a4
Thibaud <thibaud-gogs.10.isildir@recursor.net>
2018-05-30 19:36:06 +0200

    Add sample use case in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using spnavwrapper;
+
 namespace spnavcsharpwrapper
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			SpaceNav.Instance.Threshold = 5;
+			SpaceNav.Instance.Sensitivity = 0.1;
+
+			for (; ; )
+			{
+				var ev = SpaceNav.Instance.WaitEvent(100);
+				Console.WriteLine(ev);
+			}
 		}
 	}
 }
